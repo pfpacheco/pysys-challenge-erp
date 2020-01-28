b6916c5ed8b6af399d72746956798782036e67e2
Paulo Francesco Pacheco <pfpacheco@gmail.com>
2020-01-27 23:26:09 -0300

    adjusting service to return a response exactly required

--- pysys-challenge-app/src/app/api/services.py
+++ pysys-challenge-app/src/app/api/services.py
@@ -1,6 +1,7 @@
 import requests
 from flask import Request, Response, make_response
 from flask_cors import cross_origin
+from http import HTTPStatus
 
 from src.app.pysys_app import app
 
@@ -23,10 +24,11 @@ def search_for_cnpj(request: Request, response: Response):
 
     if cnpj is not None and cnpj != "":
         res = requests.get(url=url + cnpj, params=headers)
-        response = make_response(res.json(), 200)
-        res.close()
+        response.set_data(res.json())
+        response.status = HTTPStatus.OK
     else:
         message = {"cnpj": cnpj, "error": "The request does not produced any result"}
-        response = make_response(message, 400)
+        response.set_data(message)
+        response.status = HTTPStatus.BAD_REQUEST
     return response
 
