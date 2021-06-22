6855810afeec520a652887cc06f82daffe19e904
ppacheco <ppacheco@MBP-de-ppacheco.home>
2021-06-22 19:41:59 -0300

    inserting new adjustments for sped efd

--- pysys-challenge-app/src/com/pysys/api/api.py
+++ pysys-challenge-app/src/com/pysys/api/api.py
@@ -25,7 +25,10 @@ def get_cnpj(cnpj):
         response = make_response(res.json(), 200)
         res.close()
     else:
-        message = {'error': 'Could not retrieve information about the CNPJ: %s' % cnpj}
+        message = \
+            {
+                "error": "Could not retrieve information about the CNPJ: %s" % cnpj
+            }
         response = make_response(message, 500)
     return response
 
@@ -38,7 +41,13 @@ def add_person():
     if request.data:
         person = loads(request.data).get("person")
     else:
-        return make_response({"code": 500, "status": "error", "message": "Empty request is not allowed!"})
+        return make_response(
+            {
+                "code": 500,
+                "status": "error",
+                "message": "Empty request is not allowed!"
+            }
+        )
 
     message = {}
     person_db = Person()
@@ -52,13 +61,30 @@ def add_person():
             db.session.add(person_db)
             db.session.commit()
 
-            message.update({"code": 200, "status": "ok", "message": "person number %s was successful inserted - %s" % (person_db.id, person_db.__repr__())})
+            message.update(
+                {
+                    "code": 200,
+                    "status": "ok",
+                    "message": "person number %s was successful inserted - %s" % (person_db.id, person_db.__repr__())
+                }
+            )
 
         else:
-            message.update({"code": 500, "status": "error", "message": "Empty request is not allowed!"})
+            message.update(
+                {
+                    "code": 500,
+                    "status": "error", "message": "Empty request is not allowed!"
+                }
+            )
 
     except Exception as e:
-        message.update({"code": 500, "status": "error", "message": "Error adding a new person: %s" % str(e.__cause__)})
+        message.update(
+            {
+                "code": 500,
+                "status": "error",
+                "message": "Error adding a new person: %s" % str(e.__cause__)
+            }
+        )
 
     return make_response(message)
 
@@ -77,16 +103,34 @@ def delete_person(person_id):
             db.session.delete(person_db)
             db.session.flush()
 
-            message.update({"code": 200, "status": "ok", "message": "Person id number %s was successful deleted" % person_db.id})
+            message.update(
+                {
+                    "code": 200,
+                    "status": "ok",
+                    "message": "Person id number %s was successful deleted" % person_db.id
+                }
+            )
 
         except Exception as e:
-            message.update({"code": 500, "status": "error", "message": str(e)})
+            message.update(
+                {
+                    "code": 500,
+                    "status": "error",
+                    "message": str(e)
+                }
+            )
 
         finally:
             db.session.commit()
 
     else:
-        message.update({"code": 500, "status": "error", "message": "Person id number is required!"})
+        message.update(
+            {
+                "code": 500,
+                "status": "error",
+                "message": "Person id number is required!"
+            }
+        )
 
     return make_response(message)
 
@@ -105,12 +149,30 @@ def get_person(person_id):
             db.session.query(Person).filter_by(id=person_id).all()
             db.session.commit()
 
-            message.update({"code": 200, "status": "ok", "message": "Person id number %s was successful deleted" % person_db.id})
+            message.update(
+                {
+                    "code": 200,
+                    "status": "ok",
+                    "message": "Person id number %s was successful deleted" % person_db.id
+                }
+            )
 
         except Exception as e:
-            message.update({"code": 500, "status": "error", "message": str(e)})
+            message.update(
+                {
+                    "code": 500,
+                    "status": "error",
+                    "message": str(e)
+                }
+            )
     else:
-        message.update({"code": 500, "status": "error", "message": "Person id number is required!"})
+        message.update(
+            {
+                "code": 500,
+                "status": "error",
+                "message": "Person id number is required!"
+            }
+        )
 
     return make_response(message)
 
